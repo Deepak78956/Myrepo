d574ac4d7d03995c971b3811ac45e18eb74df7d2
Deepak <dk7895625828@gmail.com>
2020-01-27 22:07:11 +0530

    added new features

--- dice_game/resources/script.js
+++ dice_game/resources/script.js
@@ -10,26 +10,29 @@ GAME RULES:
 */
 
 var dice,
+  diceOne,
+  numOne,
   diceDOM = document.querySelector(".dice"),
+  diceOneDOM = document.querySelector(".dice-1"),
   roundScore,
   activePlayer,
   score,
-  gamePlaying;
+  gamePlaying,
+  sixes,
+  num,
+  limit;
 
 init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   if (gamePlaying) {
-    dice = Math.floor(Math.random() * 6) + 1;
-    diceDOM.style.display = "block";
-    diceDOM.src = "/resources/img/dice-" + dice + ".png";
-    if (dice !== 1) {
-      roundScore += dice;
-      document.querySelector(
-        "#current-" + activePlayer
-      ).textContent = roundScore;
-    } else {
+    dice = roll();
+    if (dice[0] === 1 && dice[1] === 1) {
+      addScore(dice[0], dice[1]);
+    } else if (dice[0] === 1 || dice[1] === 1) {
       nextPlayer();
+    } else {
+      addScore(dice[0], dice[1]);
     }
   }
 });
@@ -39,9 +42,10 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     score[activePlayer] += roundScore;
     document.querySelector("#score-" + activePlayer).textContent =
       score[activePlayer];
-    if (score[activePlayer] >= 100) {
+    if (score[activePlayer] >= limit) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner";
       diceDOM.style.display = "none";
+      diceOneDOM.style.display = "none";
       document
         .querySelector(".player-" + activePlayer + "-panel")
         .classList.add("winner");
@@ -64,10 +68,13 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
   diceDOM.style.display = "none";
+  diceOneDOM.style.display = "none";
 }
 function init() {
   (roundScore = 0), (activePlayer = 0), (score = [0, 0]), (gamePlaying = true);
+  limit = prompt("Enter desired limit");
   diceDOM.style.display = "none";
+  diceOneDOM.style.display = "none";
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
   document.getElementById("current-0").textContent = "0";
@@ -79,4 +86,24 @@ function init() {
   document.querySelector(".player-0-panel").classList.remove("active");
   document.querySelector(".player-0-panel").classList.add("active");
   document.querySelector(".player-1-panel").classList.remove("active");
+}
+function roll() {
+  num = Math.floor(Math.random() * 6) + 1;
+  numOne = Math.floor(Math.random() * 6) + 1;
+  diceDOM.style.display = "block";
+  diceOneDOM.style.display = "block";
+  diceDOM.src = "/resources/img/dice-" + num + ".png";
+  diceOneDOM.src = "/resources/img/dice-" + numOne + ".png";
+  if (num === 6 && numOne === 6) {
+    score[activePlayer] = 0;
+    document.getElementById("score-" + activePlayer).textContent = "0";
+    alert("Double Sixes Player -" + (activePlayer + 1));
+    return [1, 4];
+  }
+  return [num, numOne];
+}
+
+function addScore(dice, diceOne) {
+  roundScore += dice + diceOne;
+  document.querySelector("#current-" + activePlayer).textContent = roundScore;
 }
